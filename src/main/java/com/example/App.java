007b3b71cf6d7ec51c5d0221bfc568f5622ee74f
thanhtran92 <tranthanhus92@gmail.com>
2015-11-29 21:40:52 +0700

    Update version of JPA from 1 to 2.1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  */
 //@Controller
-@EnableAutoConfiguration
+@SpringBootApplication
 public class App {
     /**
      * @return "Hello World!" string
--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,11 @@ import com.example.service.ProductService;
 @Controller
 public class ProductController {
     private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
 
     @RequestMapping(value = "/products", method = RequestMethod.GET)
     public String list(Model model){
