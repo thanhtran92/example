1cc3082f3a3481770b461d4d3daaed4ebd920080
thanhtran92 <tranthanhus92@gmail.com>
2015-12-12 10:15:54 +0700

    Render views using Bootstrap

--- src/main/java/com/example/configuration/RepositoryConfiguration.java
+++ src/main/java/com/example/configuration/RepositoryConfiguration.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration
-@EnableAutoConfiguration
-@EntityScan(basePackages = {"com.example.domain"})
-@EnableJpaRepositories(basePackages = {"com.example.repository"})
-@EnableTransactionManagement
+//@Configuration
+//@EnableAutoConfiguration
+//@EntityScan(basePackages = {"com.example.domain"})
+//@EnableJpaRepositories(basePackages = {"com.example.repository"})
+//@EnableTransactionManagement
 public class RepositoryConfiguration {
 
 }
