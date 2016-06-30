c6214d6bbf61077a765e4a09c9bda54a87bd2c32
thanhtran92 <tranthanhus92@gmail.com>
2016-06-30 10:59:38 +0700

    Update sql

--- database/scripts/online_store_20_11_2015.sql
+++ database/scripts/online_store_20_11_2015.sql
@@ -12,3 +12,18 @@ create table products (
     quantity tinyint(16),
     info text
 );
+
+create table users (
+    username varchar(64) not null,
+    password varchar(64) not null,
+    enabled tinyint not null default 1 ,
+    primary key (username));
+
+create table user_roles (
+    user_role_id int(16) not null auto_increment,
+    username varchar(64) not null,
+    role varchar(45) not null,
+    primary key (user_role_id),
+    unique key uni_username_role (role, username),
+    key fk_username_idx (username),
+    constraint fk_username foreign key (username) references users (username));
